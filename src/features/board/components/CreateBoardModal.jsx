f593aee2891b47f00e8c9d8a3d9a6f70964a6f95
shubham vairagade <shubhamvairagade91@gmail.com>
2026-06-07 17:48:39 +0530

    feat: implement board background color selector

--- src/features/board/components/CreateBoardModal.jsx
+++ src/features/board/components/CreateBoardModal.jsx
@@ -1,31 +1,23 @@
+import { useState } from "react";
 import Modal from "../../../components/Modal";
+import BoardColorSelector from "./BoardColorSelector";
 
-const CreateBoardModal = ({ isOpen, onClose, boardColors }) => {
+const CreateBoardModal = ({ isOpen, onClose, boardColors,handleCreateBoard, boardForm, setBoardForm}) => {
+    const handleColorSelect = (color) =>{
+        setBoardForm((prev)=>({...prev,selectedColor:color}));
+    }
     return (
         <Modal title="Create New Board" isOpen={isOpen} onClose={onClose}>
             <div className="space-y-4">
                 <div>
                     <label className="block mb-1 text-md" htmlFor="title">Board Title</label>
-                    <input className="w-full outline-none bg-slate-700 p-2 rounded-lg" type="text" placeholder="eg. Marketing campaign Q4" />
+                    <input value={boardForm.title} onChange={(e)=>setBoardForm(prev=>({...prev,title:e.target.value}))} className="w-full outline-none bg-slate-700 p-2 rounded-lg" type="text" placeholder="eg. Marketing campaign Q4" />
                 </div>
-
-                <div>
-                    <label className="block mb-3 text-md">Board Background </label>
-                    <div className="grid grid-cols-4 gap-2 sm:grid-cols-8">
-                        {
-                            boardColors.map(color=>(
-                                <button key={color.id} className={`${color.twClass} h-10 w-10 rounded-lg`}>
-                                
-                                </button>
-                            ))
-
-                        }
-                    </div>
-                </div>
+                <BoardColorSelector boardColors={boardColors} selectedColor={boardForm.selectedColor} handleColorSelect={handleColorSelect}/>
 
                 <div className="flex gap-4 mt-10">
                     <button className="bg-red-500 p-2 rounded-lg" onClick={onClose}>Cancel</button>
-                    <button className="bg-blue-500 p-2 rounded-lg">Create Board</button>
+                    <button onClick={handleCreateBoard} className="bg-blue-500 p-2 rounded-lg">Create Board</button>
                 </div>
             </div>
         </Modal>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,34 +2,46 @@ import { SquarePlus } from "lucide-react";
 import BoardContainer from "../features/board/components/BoardContainer";
 import useBoard from "../features/board/hooks/useBoard";
 import { boardActions } from "../features/board/constant/boardActions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CreateBoardModal from "../features/board/components/CreateBoardModal";
 import { BOARD_COLORS } from "../features/board/constant/boardColors";
-const HomePage = () =>{
-    const {state, dispatch} = useBoard();
-    const [title, setTitle] = useState("");
+const HomePage = () => {
+    const { state, dispatch } = useBoard();
     const [modalOpen, setModalOpen] = useState(false);
-    const [colorId, setColorId] = useState(BOARD_COLORS[0].id)
 
-    const handleCreateBoard = () =>{
+    const [boardForm, setBoardForm] = useState({
+        title:"",
+        selectedColor:BOARD_COLORS[0],
+    })
+    const handleCreateBoard = () => {
+        if(!boardForm.title.trim()) return;
         dispatch({
-            type:boardActions.CREATE_BOARD,
-            payload:{
-                id:crypto.randomUUID(),
-                title,
-                color:"bg-pink-600",
-                listIds:[],
+            type: boardActions.CREATE_BOARD,
+            payload: {
+                id: crypto.randomUUID(),
+                title:boardForm.title,
+                color: boardForm.selectedColor.twClass,
+                listIds: [],
             }
         })
+        setModalOpen(false);
+        setBoardForm({
+            title:"",
+            selectedColor:BOARD_COLORS[0],
+        })
     }
-    return(
+
+    return (
         <div>
             <div className="flex items-center justify-between mb-8">
                 <h1 className="text-4xl font-bold ">Project Overview</h1>
-                <button onClick={()=>setModalOpen(true)} className="text-green-500"><SquarePlus size={40} strokeWidth={1}/></button>
+                <button onClick={() => setModalOpen(true)} className="text-green-500"><SquarePlus size={40} strokeWidth={1} /></button>
             </div>
-            <BoardContainer boards={state?.boards}/>
-            <CreateBoardModal isOpen={modalOpen} onClose={()=>setModalOpen(false)} boardColors={BOARD_COLORS}/>
+            <BoardContainer boards={state?.boards} />
+            <CreateBoardModal
+                handleCreateBoard={handleCreateBoard}
+                isOpen={modalOpen} onClose={() => setModalOpen(false)}
+                boardColors={BOARD_COLORS} boardForm={boardForm} setBoardForm={setBoardForm}/>
         </div>
     )
 }
